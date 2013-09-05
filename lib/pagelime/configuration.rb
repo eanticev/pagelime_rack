dfa5ef6e47452e2c020f4115086811bbc358ba1e
Joel Van Horn <joel@joelvanhorn.com>
2013-09-04 23:35:24 -0400

    Added logger and removed client-specific configuration check

--- lib/pagelime/configuration.rb
+++ lib/pagelime/configuration.rb
@@ -2,15 +2,28 @@ module Pagelime
   class Configuration
     
     attr_accessor :account_key, :account_secret, :api_version
+    attr_accessor :logger
     
     def initialize
+      
+      # client settings
+      
       account_key     = ENV['PAGELIME_ACCOUNT_KEY']
       account_secret  = ENV['PAGELIME_ACCOUNT_SECRET']
       api_version     = ENV['PAGELIME_RACK_API_VERSION']
+      
+      # global settings
+      
+      logger          = self.class.default_logger
     end
     
-    def configured?
-      !(account_key.nil? || account_secret.nil? || api_version.nil?)
+    module ClassMethods
+      def default_logger
+        @default_logger ||= Logger.new(STDOUT)
+      end
     end
+    
+    extend ClassMethods
+    
   end
 end
