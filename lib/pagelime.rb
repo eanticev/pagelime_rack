8e994a7ccfb4e1af33831c7dc35de1191fc060b8
Joel Van Horn <joel@joelvanhorn.com>
2013-09-05 01:29:02 -0400

    Simplified Pagelime module

--- lib/pagelime.rb
+++ lib/pagelime.rb
@@ -1,12 +1,10 @@
 require_relative 'pagelime/configuration'
-require_relative 'pagelime/client'
-require_relative 'pagelime/html_processor'
 
 module Pagelime
   module ClassMethods
     # Use as Pagelime.configure{|config| config.account_key = ... }
     def configure(&block)
-      yield config
+      config.configure(&block)
     end
     
     def config
@@ -14,15 +12,15 @@ module Pagelime
     end
     
     def logger
-      config.logger ||= Configuration.default_logger
+      config.logger
     end
     
     def client
-      @client ||= Client.new(config)
+      config.client
     end
     
-    def html_processor
-      @html_processor ||= HtmlProcessor.new(client)
+    def processor
+      config.processor
     end
   end
   
