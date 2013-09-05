9b6b27251683bd29350362fbbbb648a9823ea639
Joel Van Horn <joel@joelvanhorn.com>
2013-09-05 01:28:24 -0400

    Configuration now creates client and processor instances based on classes specified (to make interchangeable)

--- lib/pagelime/configuration.rb
+++ lib/pagelime/configuration.rb
@@ -1,29 +1,41 @@
+require 'logger'
+
 module Pagelime
   class Configuration
     
-    attr_accessor :account_key, :account_secret, :api_version
-    attr_accessor :logger
+    # only allow getter access unless using configure block
+    attr_accessor :account_key, :account_secret, :api_version, :client_class, :processor_class, :logger
+    attr_reader :client, :processor
     
-    def initialize
+    # pass in a configure block to write new values
+    def initialize(defaults = {}, &block)
+      @account_key     = ENV['PAGELIME_ACCOUNT_KEY']
+      @account_secret  = ENV['PAGELIME_ACCOUNT_SECRET']
+      @api_version     = ENV['PAGELIME_RACK_API_VERSION']
+      @logger          = Logger.new(STDOUT)
       
-      # client settings
-      
-      account_key     = ENV['PAGELIME_ACCOUNT_KEY']
-      account_secret  = ENV['PAGELIME_ACCOUNT_SECRET']
-      api_version     = ENV['PAGELIME_RACK_API_VERSION']
-      
-      # global settings
-      
-      logger          = self.class.default_logger
+      configure(&block)
     end
     
-    module ClassMethods
-      def default_logger
-        @default_logger ||= Logger.new(STDOUT)
+    def configure(&block)
+      if block_given?
+        # pass self to configuration block
+        yield(self)
+        
+        # recreate instances in case classes were updated
+        rebuild!
       end
+      
+      self
     end
     
-    extend ClassMethods
-    
+    def rebuild!
+      if client_class.nil? || processor_class.nil?
+        raise "You must specify client_class and processor_class"
+      end
+      
+      @client     = client_class.new(account_key, account_secret, api_version)
+      @processor  = processor_class.new(@client)
+    end
   end
 end
