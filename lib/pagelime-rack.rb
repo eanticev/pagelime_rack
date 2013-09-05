ce77c0e2059e864600d70bcc0f5a06f2b0035a0f
Joel Van Horn <joel@joelvanhorn.com>
2013-09-05 01:29:16 -0400

    Use configure block to setup defaults

--- lib/pagelime-rack.rb
+++ lib/pagelime-rack.rb
@@ -1,2 +1,9 @@
-require 'pagelime'
-require 'rack/pagelime'
+require_relative 'pagelime'
+require_relative 'pagelime/s3_client'
+require_relative 'pagelime/xml_processor'
+require_relative 'rack/pagelime'
+
+Pagelime.configure do |config|
+  config.client_class     = Pagelime::S3Client
+  config.processor_class  = Pagelime::XmlProcessor
+end
