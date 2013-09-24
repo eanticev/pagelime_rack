7ebcff90e76a2434e9aa1716f855bb00624857de
Joel Van Horn <joel@joelvanhorn.com>
2013-09-24 15:09:52 -0400

    Added the ability to customize routing path prefix and whether to enable HTML processing at the request level

--- lib/pagelime/configuration.rb
+++ lib/pagelime/configuration.rb
@@ -4,12 +4,15 @@ module Pagelime
   class Configuration
     
     # only allow getter access unless using configure block
-    attr_accessor :logger, :storage, :cache, :processor
+    attr_accessor :logger, :storage, :cache, :processor, :url_path, :toggle_processing
     attr_accessor :generate_region_cache_key, :static_shared_cache_key, :cache_fetch_options
     
     # pass in a configure block to write new values
     def initialize(defaults = {}, &block)
-      @logger = Logger.new(STDOUT)
+      @logger             = Logger.new(STDOUT)
+      @url_path           = "/pagelime"
+      # on, per_request, off
+      @toggle_processing  = "on"
       
       configure(&block)
     end
